cf3211691e04ae2bb64ea32b60510bd6c2ef11ce
Machiko Yasuda <machikoyasuda@gmail.com>
2013-12-07 14:34:53 -0800

    Set up Leaflet map

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,11 @@
 console.log('\'Allo \'Allo!');
+
+// Load Stamen map, toner layer
+var layer = new L.StamenTileLayer("toner");
+
+// Center map in Los Angeles, zoomed into level 12
+var map = new L.Map("map", {
+    center: new L.LatLng(34.071781, -118.291664),
+    zoom: 12
+});
+map.addLayer(layer);
